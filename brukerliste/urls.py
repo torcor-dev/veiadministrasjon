a35ca42daf2a78b9b1ff7991d331c134fc836ab6
Tormod Fossum <fossum@pm.me>
2020-07-17 18:01:09 +0200

    Added new user registration, and simple detailview

--- brukerliste/urls.py
+++ brukerliste/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import HytteListView
+
+from .views import BrukerDetailView, BrukerListView, HytteListView, NyBruker
 
 urlpatterns = [
-        path('', HytteListView.as_view(), name='Hytteliste'),
-        ]
+    path("", BrukerListView.as_view(), name="brukerliste"),
+    path("bruker/<int:pk>/", BrukerDetailView.as_view(), name="bruker-detail"),
+    path("hytter/", HytteListView.as_view(), name="hytteliste"),
+    path("ny_bruker/", NyBruker, name="ny-bruker"),
+]
--- brukerliste/views.py
+++ brukerliste/views.py
@@ -1,11 +1,38 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.views.generic import ListView
-from .models import Bruker, Hytte, Poststed, Faktura
+from django.views.generic import ListView, DetailView
+
+from .forms import NyBrukerForm
+from .models import Bruker, Faktura, Hytte, Poststed
+
 
 class HytteListView(ListView):
     model = Hytte
-    context_object_name = 'hytter'
+    context_object_name = "hytter"
 
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
         return ctx
+
+
+class BrukerListView(ListView):
+    model = Bruker
+    context_object_name = "brukere"
+    ordering = ["etternavn"]
+
+
+class BrukerDetailView(DetailView):
+    model = Bruker
+
+
+def NyBruker(request):
+    # TODO Valg mellom ny hytte eller overtakelse av annen hytte
+    if request.method == "POST":
+        bform = NyBrukerForm(request.POST)
+        if bform.is_valid():
+            bform.save()
+            return HttpResponseRedirect("../")
+    else:
+        bform = NyBrukerForm()
+
+    return render(request, "brukerliste/ny_bruker.html", {"bform": bform,},)
