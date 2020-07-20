d05cba0c2232537fb9f87bf125f6e5f86afaa130
Tormod Fossum <fossum@pm.me>
2020-07-21 01:26:04 +0200

    Made admin view more detailed for Bruker

--- brukerliste/admin.py
+++ brukerliste/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 
-from .models import (Adresse, Bruker, Faktura, FakturaLinje, Hytte, Poststed,
-                     Pris, Telefonnr, TidligereEier)
+from .models import (
+    Adresse,
+    Bruker,
+    Faktura,
+    FakturaLinje,
+    Hytte,
+    Poststed,
+    Pris,
+    Telefonnr,
+    TidligereEier,
+)
 
-admin.site.register(Bruker)
 admin.site.register(Hytte)
 admin.site.register(Poststed)
 admin.site.register(Faktura)
@@ -12,3 +20,15 @@ admin.site.register(Adresse)
 admin.site.register(Telefonnr)
 admin.site.register(Pris)
 admin.site.register(TidligereEier)
+
+
+@admin.register(Bruker)
+class BrukerAdmin(admin.ModelAdmin):
+    list_display = ("etternavn", "fornavn", "epost", "broyting", "faktureres")
+    list_filter = ("faktureres", "broyting")
+    search_fields = ("etternavn",)
+
+    # other neat fields:
+    # raw_id_fields = ('foreign key')
+    # ordering = ('etternavn', 'fornavn')
+
