bb08ffc5390b7b65308614d9a5c8b1329809f690
Tormod Fossum <fossum@pm.me>
2020-07-25 22:29:30 +0200

    removed invoice related models

--- brukerliste/admin.py
+++ brukerliste/admin.py
@@ -3,22 +3,16 @@ from django.contrib import admin
 from .models import (
     Adresse,
     Bruker,
-    Faktura,
-    FakturaLinje,
     Hytte,
     Poststed,
-    Pris,
     Telefonnr,
     TidligereEier,
 )
 
 admin.site.register(Hytte)
 admin.site.register(Poststed)
-admin.site.register(Faktura)
-admin.site.register(FakturaLinje)
 admin.site.register(Adresse)
 admin.site.register(Telefonnr)
-admin.site.register(Pris)
 admin.site.register(TidligereEier)
 
 
