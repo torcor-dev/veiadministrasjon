8af8d8e4ccb09effa24dffe2752748bba3e6d746
Tormod Fossum <fossum@pm.me>
2020-07-25 22:27:43 +0200

    added sorting to the table and some css

--- fosseterveien/urls.py
+++ fosseterveien/urls.py
@@ -13,10 +13,30 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
+from django.conf.urls.static import static
+
+# from django.contrib.sitemaps.views import sitemap
+# from brukerliste.sitemaps import BrukerSitemap
+
+# sitemaps = {
+#     "brukere": BrukerSitemap,
+# }
 
 urlpatterns = [
-    path('', include('brukerliste.urls')),
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("account/", include("account.urls")),
+    path("", include("brukerliste.urls")),
+    # path(
+    #     "sitemap.xml",
+    #     sitemap,
+    #     {"sitemaps": sitemaps},
+    #     name="django.contrib.sitemaps.views.sitemap",
+    # ),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
