2747f3703dc5dda3eb46b710e8fa16786bb9e8c5
Tormod Fossum <fossum@pm.me>
2020-08-10 17:25:45 +0200

    Cleanup of unused stuff

--- invoicing/urls.py
+++ invoicing/urls.py
@@ -3,7 +3,6 @@ from . import views
 from .views import FakturaDeleteView
 
 urlpatterns = [
-    path("<int:bruker_id>/", views.test_faktura, name="faktura"),
     path("<int:faktura_nr>/pdf/", views.faktura_pdf, name="faktura-pdf"),
     path("liste/", views.faktura_liste, name="faktura-liste"),
     path("utboks/slett/<int:pk>/", FakturaDeleteView.as_view(), name="faktura-slett"),
