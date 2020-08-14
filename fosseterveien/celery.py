6c8694aa075f02f159617cdc2e710f5239c0c33d
Tormod Fossum <fossum@pm.me>
2020-08-14 11:33:53 +0000

    Production settings update

--- fosseterveien/celery.py
+++ fosseterveien/celery.py
@@ -2,7 +2,7 @@ import os
 from celery import Celery
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fosseterveien.settings.base")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fosseterveien.settings.pro")
 app = Celery("fosseterveien")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
