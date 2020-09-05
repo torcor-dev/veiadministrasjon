fb282033311b3a01171d528cb74320ef8364f991
Tormod Fossum <fossum@pm.me>
2020-09-05 15:01:40 +0200

    Add admin filter

--- invoicing/admin.py
+++ invoicing/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 from .models import Faktura, FakturaLinje, Pris
 
-admin.site.register(Faktura)
 admin.site.register(FakturaLinje)
 admin.site.register(Pris)
 
-# Register your models here.
+
+@admin.register(Faktura)
+class FakturaAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "bruker",
+        "faktura_dato",
+        "betalt",
+        "sendt",
+        Faktura.get_total_sum,
+    ]
+    list_filter = ["betalt", "sendt"]
